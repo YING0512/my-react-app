0c2ef33deec6e7add8399d0e48930bd714607c72
YING0512 <5B0G0006@stust.edu.tw>
2024-05-29 20:31:19 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
       <h1>MySlider</h1>
-      <h1>5B0G0006 陳沛瑩</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
     </div>
   );
 }
